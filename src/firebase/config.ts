3444814cbb33589f14e145d1a1eda9fae4d438c4
KANISH YATHRA RAJ <Kanish.aims@gmail.com>
2025-11-05 14:32:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1386887967-ba1d0",
+  "appId": "1:206071179813:web:589ede05114345e5036dcc",
+  "apiKey": "AIzaSyCOGy4ejvlXEhwU2ShOjtgfATmMoPVC9x0",
+  "authDomain": "studio-1386887967-ba1d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "206071179813"
+};
