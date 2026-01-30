4ea2d19ce44a0a8f6655842ea8339d096e6eae92
WILSONPAULRAJ D <wilsonpaulrajd@gmail.com>
2026-01-30 01:43:14 +0000

    why the api's are not working. the api base url is not pointing to the f

--- next.config.ts
+++ next.config.ts
@@ -40,14 +40,6 @@ const nextConfig: NextConfig = {
         "https://6000-firebase-studio-1761724252944.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
